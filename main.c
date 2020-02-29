24de7ab3beb76982ebbbf68a1aa38e6d7e81c6a9
Jake Palmer <jake.palmer1842@gmail.com>
2020-02-28 17:20:02 -0800

    Fix indenting in main.c

--- main.c
+++ main.c
@@ -64,89 +64,89 @@ int main(int argc, char** argv) {
 
   // start encryption loop over blocks in the file
   while (getblock(readfd, word)) {
-  if (DEBUG) {
-    for (int i = 0; i < 4; i++) {
-      printf("word[%d]: %hu  ", i, word[i]);
-    }
-    printf("\n");
-  }
-
-  // TEMPORARY WORDS FOR TESTING
-  if (encrypt) {
-    word[0] = 0x0123;
-    word[1] = 0x4567;
-    word[2] = 0x89ab;
-    word[3] = 0xcdef;
-  } else {
-    word[0] = 0x9a76;
-    word[1] = 0xd6d5;
-    word[2] = 0x78c4;
-    word[3] = 0x4766;
-  }
-
-  // input whitening
-  if (DEBUG) printf("Input Whitening: ");
-  whiten(word);
-
-  for (int round = 0; round < 16; round++) {
-    if (DEBUG) printf("ROUND %d\n", round);
-
-    // F function
-    unsigned short f[2];
-    f_function(word[0], word[1], round, f, encrypt);
-
-    // get temp[0]
-    if (encrypt) {
-      temp[0] = word[2] ^ f[0];
-      temp[0] = (temp[0] >> 1) | (temp[0] << 15);
-    } else {
-      temp[0] = (word[2] << 1) | (word[2] >> 15);
-      temp[0] = temp[0] ^ f[0];
-    }
-
-    // get temp[1]
-    if (encrypt) {
-      temp[1] = (word[3] << 1) | (word[3] >> 15);
-      temp[1] = temp[1] ^ f[1];
-    } else {
-      temp[1] = word[3] ^ f[1];
-      temp[1] = (temp[1] >> 1) | (temp[1] << 15);
-    }
-
-    // get temp[2]
-    temp[2] = word[0];
-
-    // get temp[3]
-    temp[3] = word[1];
-
-    // assign temps as new words for next round
-    word[0] = temp[0];
-    word[1] = temp[1];
-    word[2] = temp[2];
-    word[3] = temp[3];
-
     if (DEBUG) {
-      printf("Block: ");
       for (int i = 0; i < 4; i++) {
-        printf("%x", word[i]);
+        printf("word[%d]: %hu  ", i, word[i]);
       }
       printf("\n");
     }
-  }
 
-  // undo last swap
-  word[0] = temp[2];
-  word[1] = temp[3];
-  word[2] = temp[0];
-  word[3] = temp[1];
+    // TEMPORARY WORDS FOR TESTING
+    if (encrypt) {
+      word[0] = 0x0123;
+      word[1] = 0x4567;
+      word[2] = 0x89ab;
+      word[3] = 0xcdef;
+    } else {
+      word[0] = 0x9a76;
+      word[1] = 0xd6d5;
+      word[2] = 0x78c4;
+      word[3] = 0x4766;
+    }
 
-  // output whitening
-  if (DEBUG) printf("Output Whitening: ");
-  whiten(word);
+    // input whitening
+    if (DEBUG) printf("Input Whitening: ");
+    whiten(word);
 
-  //write cipher block to output file
+    for (int round = 0; round < 16; round++) {
+      if (DEBUG) printf("ROUND %d\n", round);
 
-  }
+      // F function
+      unsigned short f[2];
+      f_function(word[0], word[1], round, f, encrypt);
+
+      // get temp[0]
+      if (encrypt) {
+        temp[0] = word[2] ^ f[0];
+        temp[0] = (temp[0] >> 1) | (temp[0] << 15);
+      } else {
+        temp[0] = (word[2] << 1) | (word[2] >> 15);
+        temp[0] = temp[0] ^ f[0];
+      }
+
+      // get temp[1]
+      if (encrypt) {
+        temp[1] = (word[3] << 1) | (word[3] >> 15);
+        temp[1] = temp[1] ^ f[1];
+      } else {
+        temp[1] = word[3] ^ f[1];
+        temp[1] = (temp[1] >> 1) | (temp[1] << 15);
+      }
+
+      // get temp[2]
+      temp[2] = word[0];
+
+      // get temp[3]
+      temp[3] = word[1];
+
+      // assign temps as new words for next round
+      word[0] = temp[0];
+      word[1] = temp[1];
+      word[2] = temp[2];
+      word[3] = temp[3];
+
+      if (DEBUG) {
+        printf("Block: ");
+        for (int i = 0; i < 4; i++) {
+          printf("%x", word[i]);
+        }
+        printf("\n");
+      }
+    }
+
+    // undo last swap
+    word[0] = temp[2];
+    word[1] = temp[3];
+    word[2] = temp[0];
+    word[3] = temp[1];
+
+    // output whitening
+    if (DEBUG) printf("Output Whitening: ");
+    whiten(word);
+
+    //write cipher block to output file
+
+    }
   close(readfd);
   close(writefd);
 }
